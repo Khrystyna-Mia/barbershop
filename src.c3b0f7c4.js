ed121e0a61b4272d1a371f67d437c80ecddc0c49
Khrystyna-Mia <88985351+Khrystyna-Mia@users.noreply.github.com>
2022-04-03 19:35:03 +0000

    Deploying to gh-pages from @ Khrystyna-Mia/barbershop@544cb2b8ce33a34f6b0136ccff8261f8539498d7 🚀

--- src.c3b0f7c4.js
+++ src.c3b0f7c4.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/barbershop/src.e7a0d752.js.map
+//# sourceMappingURL=/barbershop/src.c3b0f7c4.js.map
