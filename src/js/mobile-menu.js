3ddb2fb8ec8624a439a496d8e50e136601199452
Khrystyna Hrekul <88985351+Khrystyna-Mia@users.noreply.github.com>
2022-04-27 18:06:48 +0300

    js mobile-menu

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,26 @@
+(() => {
+    const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobileMenuRef = document.querySelector("[data-menu]");
+  
+    const onMobileMenuClick = () => {
+      const expanded =
+        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+  
+      menuBtnRef.classList.toggle("is-open");
+      menuBtnRef.setAttribute("aria-expanded", !expanded);
+  
+      mobileMenuRef.classList.toggle("is-open");
+      document.body.classList.toggle("modal-open");
+    };
+  
+    const onMobileMenuLinkClick = (event) => {
+      if (!event.target.hasAttribute("data-mobile-link")) {
+        return;
+      }
+      onMobileMenuClick();
+    };
+  
+    menuBtnRef.addEventListener("click", onMobileMenuClick);
+    mobileMenuRef.addEventListener("click", onMobileMenuLinkClick);
+  })();
+    
